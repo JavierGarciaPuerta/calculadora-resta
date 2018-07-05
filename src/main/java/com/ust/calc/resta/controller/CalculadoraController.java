4c3586dd23d8959ec3c1bef02de4d8121250894e
afernandez <abelfdezgomez@gmail.com>
2018-07-05 13:04:14 +0200

    Test sobre la implementacion del Servicio Calculadora

--- src/main/java/com/ust/calc/resta/controller/CalculadoraController.java
+++ src/main/java/com/ust/calc/resta/controller/CalculadoraController.java
@@ -1,10 +1,21 @@
 package com.ust.calc.resta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.ust.calc.resta.service.Calculadora;
+
+@RestController
 public class CalculadoraController {
 	
-	public Integer realizarResta(Integer uno, Integer dos) {
-		return 1;
+	@Autowired
+	private Calculadora calculadora;
+	
+	@GetMapping("/resta/{uno}/{dos}")
+	public Integer realizarResta(@PathVariable Integer uno, @PathVariable Integer dos) {
+		return calculadora.resta(uno, dos);
 	}
 
 }
