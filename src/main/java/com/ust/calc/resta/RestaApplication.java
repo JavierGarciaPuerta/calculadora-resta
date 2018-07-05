43b825a9da4f520d6c103269dde72579ec88c558
Javier <javiergarciapuerta@gmail.com>
2018-07-05 10:53:19 +0200

    creación del proyecto maven

---
+++ src/main/java/com/ust/calc/resta/RestaApplication.java
@@ -0,0 +1,12 @@
+package com.ust.calc.resta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaApplication.class, args);
+	}
+}
